3c3041ddea3b0427c2578e234d297bfc4976598c
pidlv <pidlv@DESKTOP-AUDCIU3.home>
2020-05-28 16:37:11 +0200

    Initialize project

---
+++ src/vue/LabyConsole.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vue;
+import java.util.Scanner;
+import modele.Case;
+import modele.Labyrinthe;
+import modele.CaseMur;
+import modele.CaseTrou;
+
+/**
+ *
+ * @author pidlv
+ */
+public class LabyConsole {
+   public LabyConsole()
+   {
+   }
+   
+    
+    public int menu() 
+    {
+        Scanner entree =   new Scanner(System.in);
+        String choix = entree.nextLine();
+        while( (Integer.parseInt(choix) < 0 ) || (Integer.parseInt(choix) >2 )) {
+            System.out.println("1-Deplacer automatiquement en profondeur");
+            System.out.println("2-Deplacement aléatoire");
+            System.out.println("0-quitter le programme ");
+            choix = entree.nextLine();
+        }
+        return Integer.parseInt(choix);
+    }
+    
+    
+    public void affiche(Case c) 
+    {
+        System.out.println(c.getPositionX() + ',' + c.getPositionY());
+         
+   }
+    
+    public void affiche(Labyrinthe laby) 
+    {
+        for(int caseIndex=0; caseIndex < laby.getGrille().size(); caseIndex++){
+            if (laby.getGrille().get(caseIndex) instanceof CaseTrou) {
+                System.out.println('_');
+            }
+            if (laby.getGrille().get(caseIndex) instanceof CaseMur ) {
+                System.out.println('X');
+            }
+        }
+    } 
+}
+
